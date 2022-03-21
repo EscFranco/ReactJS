b055acb64ca0658b5f2d1c9f4104d08feca987d2
Franco Escobar <escobarfranco95@gmail.com>
2022-03-21 16:56:38 -0300

    Creacion NavBar

---
+++ proyectoescobar/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+function NavBar () {
+    return (
+        <header>
+            <h1> Tienda Online </h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Informacion</a></li>
+                <li><a href="#">Hombres</a></li>
+                <li><a href="#">Mujeres</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default NavBar
