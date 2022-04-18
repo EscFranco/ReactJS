00c17d5c0af1b8ea2a039279b9463b30b102fc3b
Franco Escobar <escobarfranco95@gmail.com>
2022-04-18 19:40:11 -0300

    CartView condiciones y Total

--- src/components/Cart/Carrito.jsx
+++ src/components/Cart/Carrito.jsx
@@ -1,31 +1,41 @@
 import { cartContext } from "../Context/CartContext.jsx";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const Carrito = () => {
 
-	const { clearCart } = useContext(cartContext);
-	const { carrito } = useContext(cartContext);
-	const { delProduct } = useContext(cartContext)
+	const { clearCart, carrito, delProduct, totalCarrito } = useContext(cartContext);
 
-    return (
+	return (
 		<>
-			<h1>Esta es su orden</h1>
-			{carrito.map ((element) => {
-				return (
-					<div key={element.id}>
-						<img src={element.image} alt="HOLA!"></img>
-						<p>Nombre: {element.title}</p>
-						<p>Valor: ${element.precio}</p>
-						<p>Cantidad: {element.cantidad}</p>						
-						<button onClick={() => delProduct(element.id)}>Eliminar Producto</button>
-					</div>
-				)
-			})}
-			<div>
-				<button onClick={clearCart}>Vaciar Carrito</button>
-			</div>
+			{carrito.length > 0 ? (
+				<div>
+					<h1>Esta es su orden</h1>
+					{carrito.map((element) => {
+						return (
+							<div key={element.id}>
+								<img src={element.image} alt="HOLA!"></img>
+								<p>Nombre: {element.title}</p>
+								<p>Valor: ${element.precio}</p>
+								<p>Cantidad: {element.cantidad}</p>
+								<button onClick={() => delProduct(element.id)}>Eliminar Producto</button>
+							</div>
+						)
+					})}
+					<p>TOTAL: ${totalCarrito}</p>
+					<button onClick={clearCart}>Vaciar Carrito</button>
+					<button>Terminar compra</button>
+				</div>
+
+			) : (
+				<div>
+					<h1> Su carrito esta vacio</h1>
+					<Link to="/">Volver a la pagina principal</Link>
+				</div>
+			)
+			}
 		</>
-    )
+	)
 };
 
 export default Carrito;
